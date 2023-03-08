0acac135206e12852ed12a83e5e5fbd717c6ab64
brian yobra <briannjoroge89@gmail.com>
2023-03-08 06:31:51 -0800

    Initial Commit

---
+++ hello_world/hello_world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello_world
+//
+//  Created by Brian The Original Yobra on 08/03/2023.
+//  Copyright © 2023 Brian The Original Yobra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
